3aa3c47524b927f5fdf604319a52e564e55b3c8f
Johan Bos <johan.bos@c6.eu>
2017-07-18 09:13:12 +0200

    Fix import due to manual edit from github

--- src/main/java/ch/qos/logback/core/rolling/S3TimeBasedRollingPolicy.java
+++ src/main/java/ch/qos/logback/core/rolling/S3TimeBasedRollingPolicy.java
@@ -21,10 +21,12 @@ import ch.qos.logback.core.rolling.aws.AmazonS3ClientImpl;
 import ch.qos.logback.core.rolling.shutdown.RollingPolicyShutdownListener;
 import ch.qos.logback.core.rolling.shutdown.ShutdownHookType;
 import ch.qos.logback.core.rolling.shutdown.ShutdownHookUtil;
+
 import java.io.File;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
